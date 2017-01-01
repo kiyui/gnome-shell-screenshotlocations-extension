cc70c06217f958053e16845910f039ab57615edb
Timur Kiyivinski <deathseclipse@gmail.com>
2017-01-01 11:32:10 +0800

    [WIP] Get GSetting value

--- extension.js
+++ extension.js
@@ -1,4 +1,11 @@
+const gio = imports.gi.Gio
+const gnomeScreenshot = new gio.Settings({
+  schema: 'org.gnome.gnome-screenshot'
+})
+
 function enable () {
+  const location = gnomeScreenshot.get_string('auto-save-directory')
+  print('Current screenshot location set to: ' + location)
 }
 
 function disable () {
