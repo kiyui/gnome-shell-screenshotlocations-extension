5868e542a4e27d89797ce0047bd6327b61ad19af
Timur Kiyivinski <deathseclipse@gmail.com>
2017-01-03 19:50:39 +0800

    [WIP] Set screenshot location

--- extension.js
+++ extension.js
@@ -3,9 +3,17 @@ const gnomeScreenshot = new gio.Settings({
   schema: 'org.gnome.gnome-screenshot'
 })
 
+function _setScreenshotLocation (location) {
+  if (gnomeScreenshot.set_string('auto-save-directory', location)) {
+    gio.Settings.sync()
+    print('Updated screenshot location to: ' + location)
+  }
+}
+
 function enable () {
   const location = gnomeScreenshot.get_string('auto-save-directory')
   print('Current screenshot location set to: ' + location)
+  _setScreenshotLocation(new Date().toLocaleString())
 }
 
 function disable () {
