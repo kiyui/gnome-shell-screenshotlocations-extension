eb36daf49125715053d578cdfb432cc757f059ba
Timur Kiyivinski <deathseclipse@gmail.com>
2017-03-10 15:25:22 +0800

    [ADD] Keybinding to extension loading utility

--- extension.js
+++ extension.js
@@ -1,9 +1,12 @@
 /* global imports print */
 
+const Me = imports.misc.extensionUtils.getCurrentExtension()
 const Main = imports.ui.main
 const Gio = imports.gi.Gio
+const GLib = imports.gi.GLib
 const Meta = imports.gi.Meta
 const Shell = imports.gi.Shell
+const Keybinder = Me.imports.keybinder.keybinder.default
 
 const defaultKeys = ['area-screenshot', 'area-screenshot-clip', 'screenshot', 'screenshot-clip', 'window-screenshot', 'window-screenshot-clip']
 
@@ -15,21 +18,20 @@ const shortcutSchema = new Gio.Settings({
   schema: 'org.gnome.settings-daemon.plugins.media-keys'
 })
 
+const keybinder = new Keybinder('screenshotlocations', GLib.get_tmp_dir())
+keybinder.add('bye-world', '<Super>j', () => print('Goodbye World!'))
+
 function enable () { // eslint-disable-line no-unused-vars
+  keybinder.enable()
   defaultKeys.map(key => {
     if (shortcutSchema.set_string(key, '')) {
       Gio.Settings.sync()
     }
   })
-
-  if (Main.wm.addKeybinding && Shell.ActionMode) {
-    Main.wm.addKeybinding('screenshot', shortcutSchema, Meta.KeyBindingFlags.NONE, Shell.ActionMode.NORMAL, function () {
-      print('This has never, will never, shall never work')
-    })
-  }
 }
 
 function disable () { // eslint-disable-line no-unused-vars
+  keybinder.disable()
   defaultKeys.map(key => {
     shortcutSchema.reset(key) // Reset all screenshot keys
   })
