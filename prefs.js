fc8b6aa6348be0a27da6490c87f984aeb38ac288
Timur Kiyivinski <deathseclipse@gmail.com>
2017-03-10 13:58:12 +0800

    [REFACTOR] Preferences

--- prefs.js
+++ prefs.js
@@ -1,39 +1,49 @@
 /* global imports print */
 
-const gio = imports.gi.Gio
-const gnomeScreenshot = new gio.Settings({
-  schema: 'org.gnome.gnome-screenshot'
-})
-
-const Gettext = imports.gettext
+const Gio = imports.gi.Gio
 const Gtk = imports.gi.Gtk
 
-Gettext.bindtextdomain('gnome-panel-3.0', '/usr/share/locale')
-Gettext.textdomain('gnome-panel-3.0')
+function buildPrefsWidget () { // eslint-disable-line no-unused-vars
+  // Get screenshot save location
+  const schema = new Gio.Settings({
+    schema: 'org.gnome.gnome-screenshot'
+  })
 
-Gtk.init(null)
+  const location = schema.get_string('auto-save-directory')
 
-const window = new Gtk.Window({ type: Gtk.WindowType.TOPLEVEL })
-const frame = new Gtk.Box({
-  orientation: Gtk.Orientation.VERTICAL,
-  border_width: 10,
-  margin: 20,
-  spacing: 20
-})
+  const vbox = new Gtk.Box({
+    orientation: Gtk.Orientation.VERTICAL,
+    border_width: 10,
+    margin: 20,
+    spacing: 20
+  })
 
-const location = gnomeScreenshot.get_string('auto-save-directory')
-const entry = new Gtk.Entry({ text: location })
+  // Create children objects
+  const entry = new Gtk.Entry({ text: location })
 
-const save = new Gtk.Button({ label: 'Save' })
-save.connect('clicked', function () {
-  print(entry.text)
-})
+  const save = new Gtk.Button({ label: 'Save' })
 
-frame.add(new Gtk.Label({ label: 'Screenshot Path' }))
-frame.add(entry)
-frame.add(save)
+  // Add objects to frame
+  vbox.add(new Gtk.Label({ label: 'Screenshot Path' }))
+  vbox.add(entry)
+  vbox.add(save)
 
-window.add(frame)
-window.show_all()
+  // Handle events
+  save.connect('clicked', function () {
+    const newLocation = entry.text
+    print('Changing screenshot location to: ' + newLocation)
 
-Gtk.main()
+    if (schema.set_string('auto-save-directory', newLocation)) {
+      Gio.Settings.sync()
+      print('Updated screenshot location to: ' + newLocation)
+      Gtk.main_quit()
+    }
+  })
+
+  vbox.show_all()
+  return vbox
+}
+
+function init () { // eslint-disable-line no-unused-vars
+  print('Initilize screenshot location extension configuration window')
+}
