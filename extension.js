6e03b63c70522f43c11c59e3bb91b147e0aa68dc
Timur Kiyivinski <deathseclipse@gmail.com>
2017-03-11 16:03:43 +0800

    [REFACTOR] dconf mappings

--- extension.js
+++ extension.js
@@ -1,38 +1,64 @@
 /* global imports print */
 
 const Me = imports.misc.extensionUtils.getCurrentExtension()
-const Main = imports.ui.main
 const Gio = imports.gi.Gio
 const GLib = imports.gi.GLib
-const Meta = imports.gi.Meta
-const Shell = imports.gi.Shell
 const Keybinder = Me.imports.keybinder.keybinder.default
 
-const defaultKeys = ['area-screenshot', 'area-screenshot-clip', 'screenshot', 'screenshot-clip', 'window-screenshot', 'window-screenshot-clip']
-
-const screenshotSchema = new Gio.Settings({
-  schema: 'org.gnome.gnome-screenshot'
-})
+const screenshotKeys = [
+  {
+    name: 'area-screenshot',
+    shortcut: '<Shift>Print',
+    command: 'gnome-screenshot -a'
+  },
+  {
+    name: 'area-screenshot-clip',
+    shortcut: '<Ctrl><Shift>Print',
+    command: 'gnome-screenshot -a -c'
+  },
+  {
+    name: 'screenshot',
+    shortcut: 'Print',
+    command: 'gnome-screenshot'
+  },
+  {
+    name: 'screenshot-clip',
+    shortcut: '<Ctrl>Print',
+    command: 'gnome-screenshot -c'
+  },
+  {
+    name: 'window-screenshot',
+    shortcut: '<Alt>Print',
+    command: 'gnome-screenshot -w'
+  },
+  {
+    name: 'window-screenshot-clip',
+    shortcut: '<Ctrl><Alt>Print',
+    command: 'gnome-screenshot -w -c'
+  }
+]
 
 const shortcutSchema = new Gio.Settings({
   schema: 'org.gnome.settings-daemon.plugins.media-keys'
 })
 
 const keybinder = new Keybinder('screenshotlocations', GLib.get_tmp_dir())
-keybinder.add('bye-world', '<Super>j', () => print('Goodbye World!'))
 
 function enable () { // eslint-disable-line no-unused-vars
-  keybinder.enable()
-  defaultKeys.map(key => {
-    if (shortcutSchema.set_string(key, '')) {
+  screenshotKeys.map(screenshotKey => {
+    if (shortcutSchema.set_string(screenshotKey.name, '')) {
       Gio.Settings.sync()
+      keybinder.add(screenshotKey.name, screenshotKey.shortcut, function () {
+        print('Running command: ' + screenshotKey.command)
+      })
     }
   })
+  keybinder.enable()
 }
 
 function disable () { // eslint-disable-line no-unused-vars
   keybinder.disable()
-  defaultKeys.map(key => {
-    shortcutSchema.reset(key) // Reset all screenshot keys
+  screenshotKeys.map(screenshotKey => {
+    shortcutSchema.reset(screenshotKey.name) // Reset all screenshot keys
   })
 }
